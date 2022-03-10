abb737220a9170e4598c824050e374e711e414b9
Ahmed <ahmadsiddiqueofficial1820665@gmail.com>
2022-03-10 21:57:45 +0500

    First java program

--- src/main/java/ch2InputOutput/Welcome1.java
+++ src/main/java/ch2InputOutput/Welcome1.java
@@ -1,6 +1,13 @@
 package ch2InputOutput;
 
 public class Welcome1 {
+    //Java first program for Learning how to print a line
+      //Developer Name: Ahmad Siddique
+    // Date: 10-March-2022
+    // Time: 21:57
+
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Java programming");
     }
---
+++ src/main/java/ch2InputOutput/Welcome4.java
@@ -0,0 +1,7 @@
+package ch2InputOutput;
+
+public class Welcome4 {
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s");
+    }
+}
