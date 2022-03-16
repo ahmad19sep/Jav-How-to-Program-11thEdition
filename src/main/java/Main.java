6e06f26728f4093c02ba0b8f01d52038a60971e4
Ahmed <ahmadsiddiqueofficial1820665@gmail.com>
2022-03-16 10:33:11 +0500

    Practice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        ModifiedClass account1=new ModifiedClass("Ahmad",434323);
-        account1.withDraws(343345343);
+        PetrolPurchase basirPur=new PetrolPurchase("Basirpur","All",1000000,150,10000,"All");
+        basirPur.setLocationPump("purani Chongi");
+        basirPur.setPetrolType("Desiel,petrol ");
+        basirPur.setNamePump("Local");
+        basirPur.setDiscount(10000);
+        basirPur.setRsPrLtr(150);
+        basirPur.setPetrolQuantity(1000);
+        System.out.println(basirPur.getNamePump());
+        System.out.println(basirPur.getLocationPump());
+        System.out.println(basirPur.getPetrolType());
+        System.out.println(basirPur.getPetrolQuantity());
+        System.out.println(basirPur.getRsPrLtr());
+        System.out.println(basirPur.getDiscount());
+        System.out.println("Total Amount you pay = "+basirPur.getPurchaseAmount());
+
     }
 }
