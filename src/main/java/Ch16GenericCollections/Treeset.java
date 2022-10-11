c167742bb804cdedfe23bae85624677e4eb1637b
Ahmed <ahmadsiddiqueofficial1820665@gmail.com>
2022-10-11 17:35:45 +0500

    TreeSet

---
+++ src/main/java/Ch16GenericCollections/Treeset.java
@@ -0,0 +1,39 @@
+package Ch16GenericCollections;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Treeset {
+    public static void main(String[] args) {
+        SortedSet<String> set = new TreeSet<>();
+        set.add("ALi");
+        set.add("Shahzad");
+        set.add("Siddique");
+        set.add("Ahmad");
+        set.add("John");
+        //Tree set print elements in ordered way like
+        System.out.println(set);
+        // Using some functions
+        System.out.println("Print first element : " + set.first());
+        System.out.println("Print last element : " + set.last());
+        System.out.println("Print before selective element" + set.headSet("John"));
+        System.out.println("Print from  selective element to end" + set.tailSet("John"));
+        set.remove("Ahmad");
+        //print elements one by one
+        for (String e : set) {
+            System.out.println(e);
+        }
+    }
+}
+/**
+ * output-:
+ * [ALi, Ahmad, John, Shahzad, Siddique]
+ * Print first element : ALi
+ * Print last element : Siddique
+ * Print before selective element[ALi, Ahmad]
+ * Print from  selective element to end[John, Shahzad, Siddique]
+ * ALi
+ * John
+ * Shahzad
+ * Siddique
+ */
