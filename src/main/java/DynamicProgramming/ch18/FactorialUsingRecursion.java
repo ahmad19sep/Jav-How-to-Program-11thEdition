864c5d52217eb0e66566f91e910ba2153c220d2c
Ahmed <ahmadsiddiqueofficial1820665@gmail.com>
2022-10-17 09:48:45 +0500

    FactorialUsingRecursion

--- src/main/java/DynamicProgramming/ch18/FactorialUsingRecursion.java
+++ src/main/java/DynamicProgramming/ch18/FactorialUsingRecursion.java
@@ -2,15 +2,24 @@ package DynamicProgramming.ch18;
 
 public class FactorialUsingRecursion {
     private static void factCalculator(int num, int fact){
-        if (num==1){
+        if (num<=1){
             System.out.printf("%s: %d","Factorial is ",fact);
             return;
         }
-        fact*=num;
-        factCalculator(num-1,fact);
+
+        factCalculator(num-1,fact*num);
     }
 
     public static void main(String[] args) {
-        factCalculator(6,1);
+        factCalculator(5,1);
+
+        System.out.printf("%n%s : %d","Factorial is",fact(5));
     }
+    //Another type of factorial using recursion
+
+    private static long fact(long num){
+        if (num<=1) return 1;
+        else return num*fact(num-1);
+    }
+
 }
